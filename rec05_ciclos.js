0fd8f6f968a9e2fb5ede0305420c025f84fe3e05
Adryan-dry <adryangalzerano00@gmail.com>
2026-09-23 16:28:44 -0300

    atualizando

--- rec05_ciclos.js
+++ rec05_ciclos.js
@@ -1,2 +1,12 @@
 //Uma esteira produz uma quantidade fixa de produtos por ciclo. O supervisor quer visualizar a produção
 // acumulada dos ciclos 1 até 12.
+const entrada = require("readline-sync");
+
+const pecasPorCiclo = entrada.questionInt("Quantas pecas a maquina produz p/ ciclo: ");
+console.log("/n===== PRODUÇÃO ACUMULADA =====");
+
+for (let ciclo = 1; ciclo <= 10; ciclo++){
+    const acumulado = pecasPorCiclo * ciclo;
+    console.log(`Ciclo${ciclo}: ${acumulado} pecas`);
+
+}
